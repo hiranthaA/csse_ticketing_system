a35206bb1853f39748810d68ddd17cfb77b118ce
hiranthaA <30893182+hiranthaA@users.noreply.github.com>
2018-09-30 02:15:08 +0530

    home component modified

--- src/components/body/content/home/home.js
+++ src/components/body/content/home/home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './home.css';
 
 class LandingPage extends Component {
     constructor(props) {
@@ -8,7 +9,26 @@ class LandingPage extends Component {
     render() { 
         return ( 
             <div>
-                this is homepage component
+                <div className="row">
+                    <div className="col-md-12">
+                        <div >
+                            <img class="img-thumbnail imghomebg" src="background.jpg" width="500" alt="Card image cap" />
+                        </div>
+                    </div>
+                </div>
+                <br/>
+                <br/>
+                <div className="row">
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <button type="button" className="btn btnJourney btn-info" >Journey</button>
+                    </div>
+                </div>
+                <br/>
+                <div className="row">
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <button type="button" className="btn btnRecharge btn-info" >Recharge Account</button>
+                    </div>
+                </div>
             </div>
         );
     }
