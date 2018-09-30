abfa3de9d678b42a16f16d5db0f9435ed006c33e
hiranthaA <30893182+hiranthaA@users.noreply.github.com>
2018-09-30 14:41:51 +0530

    digital Pass feature added

---
+++ src/components/body/content/digitalpass/digitalpass.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class DigitalPass extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                this is Digital Pass component
+            </div>
+        );
+    }
+}
+ 
+export default DigitalPass;
