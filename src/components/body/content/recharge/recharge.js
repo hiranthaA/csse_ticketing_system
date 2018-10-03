dc0ba91593b8797c459d99154308d2bba48c6b2a
Parakrama Shashitha <pshashitha@gmail.com>
2018-10-03 15:10:42 +0530

    Before SMS

--- src/components/body/content/recharge/recharge.js
+++ src/components/body/content/recharge/recharge.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import './recharge.css'
+
+const accountSid = 'ACb973d8209fc075d129ff421383aec6b1';
+const authToken = '59814d7d40309d08296b1c70e8a46a9e';
+
+const twilio = require('twilio');
+const cors = require('cors');
+var client = new twilio(accountSid, authToken);
+
 class Recharge extends Component {
     constructor(props) {
         super(props);
         this.validateVerification = this.validateVerification.bind(this);
+        this.sendCode = this.sendCode.bind(this);
         this.state = { 
             balance:60,
             account:582065002,
             code:1234
         }
+    }
+    sendCode(e){
+        // fetch("https://api.txtlocal.com/send/",{method:"POST",body:{"apiKey":apiKey,"numbers":numbers,"sender":sender,"message":message}}).then((res)=>{
+        //     if(res.status==="success"){
+        //         alert("Verification Code Sen");
+        //     }
+        // });
+       /** 
+        //TWILIO
+        process.env.MY_PHONE_NUMBER            TO STORE MY NUMBER
+
+        */
+        client.messages.create({
+            to:'+94705143507',
+            from:'+18606153924',
+            body:'Your Code: '+this.state.code
+        }).then((message)=>console.log(message.sid)).catch((err)=>console.log(err));
     }
 
     validateVerification(e){
@@ -17,12 +43,14 @@ class Recharge extends Component {
         if(e.keyCode === 13){//pressed enter
             if(e.target.value!==this.state.code.toString())
              alert("Invalid Verification!");
+             e.preventDefault();
         }
         if(e.target.value===this.state.code.toString()){
                 alert("Account Recharged");
+                
             }
 
-            e.preventDefault();
+           
     }
     render() { 
         let currentBalance="$"+this.state.balance;
@@ -75,8 +103,8 @@ class Recharge extends Component {
                                     <div class="form-group form-inline">
                                         <label class="sr-only" for="exampleInputAmount">Amount (in dollars)</label>
                                         <div class="form-text-Recharge">
-                                            <input type="number" className="rechargeInputsAmount" placeholder="0 of 0" id="ramount"/>
-                                            <label for="ramount" class="static-value-Recharge">Rs/=</label>
+                                            <input type="number" className="rechargeInputsAmount" placeholder="Ex: 100" id="ramount" min={0}/>
+                                            <label for="ramount" className="static-value-Recharge">Rs/=</label>
                                         </div>
                                     </div>
                              
@@ -87,7 +115,7 @@ class Recharge extends Component {
                         <div className="row">
 
                             <div className="col rechargeCodeClass">
-                                    <button type="submit" class="btn btn-secondary btn-lg">Get Code</button> 
+                                    <button type="submit" onSubmit={this.sendCode} class="btn btn-secondary btn-lg">Get Code</button> 
                             </div>
                         </div>
                         <div className="row">
