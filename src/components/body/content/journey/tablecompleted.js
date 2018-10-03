613b72482411b676ad8fc0edb68d7dd366354a96
hiranthaA <30893182+hiranthaA@users.noreply.github.com>
2018-10-04 04:15:56 +0530

    journey current bus location and history time display fixed

--- src/components/body/content/journey/tablecompleted.js
+++ src/components/body/content/journey/tablecompleted.js
@@ -5,8 +5,18 @@ import './tablecompleted.css';
 class TableCompleted extends Component {
     constructor(props) {
         super(props);
+        this.getJourneyDetails = this.getJourneyDetails.bind(this);
         this.state = {
-            journeylist: []
+            journeylist: [],
+            journeyid : null,
+            busid : null,
+            busroot : null,
+            jstartloc : null,
+            jendloc : null,
+            jstarttime : null,
+            jendtime : null,
+            jstatus : null,
+            jfare : null
         }
     }
 
@@ -15,13 +25,32 @@ class TableCompleted extends Component {
     }
 
     getJourneyList() {
-        Axios.get('http://localhost:9090/journey/getall?cid='+this.props.currentuser+'&status=completed').then(function (res) {
-            console.log(res.data);
+        Axios.get('http://localhost:9090/journey/getall?cid=' + this.props.currentuser + '&status=completed').then(function (res) {
+            console.log(res);
             this.setState({ journeylist: res.data });
             return res.data;
         }.bind(this)).then(function (data) {
             console.log(data);
 
+        }.bind(this));
+    }
+
+    getJourneyDetails(e) {
+        console.log("this is get journey details"+e.target.id);
+        Axios.get('http://localhost:9090/journey/getbyid/?jid='+e.target.id).then(function (res) {
+            console.log(res.data);
+            return res.data;
+        }.bind(this)).then(function (data) {
+            if(data!==""){
+                console.log("data found on passenger");
+                this.setState({ journeyid : data.journeyId });
+                this.setState({ busid : data.busId });
+                this.setState({ busroot : data.busroot });
+                this.setState({ jstartloc : data.jstartloc });
+                this.setState({ jendloc : data.jendloc });
+                this.setState({ jstatus : data.jstatus });
+                this.setState({ jfare : data.jfare });
+            }
         }.bind(this));
     }
 
@@ -36,19 +65,28 @@ class TableCompleted extends Component {
                 <table className="table table-hover table-active">
                     <tbody>
                         {this.state.journeylist.map((journey, j) => {
+                            console.log(journey);
+                            var  sdate =  new Date(journey.jstarttime);                            
+                            var startdate = sdate.toDateString();
+                            var starttime = sdate.toLocaleTimeString();
+                            var  edate =  new Date(journey.jendtime);                            
+                            var enddate = edate.toDateString();
+                            var endtime = edate.toLocaleTimeString();
+                            console.log(starttime);
+                            var  enddate =  new Date(journey.jstarttime); 
                             return (
                                 <tr key={j}>
-                                    <td className="text-left">{
+                                    <td className="text-left" id={journey.journeyId} data-toggle="modal" data-target="#viewTicketModal" onClick={this.getJourneyDetails}>{
                                         <div>
-                                            <p className="font-weight-bold text-dark">{journey.jstarttime}</p>
+                                            <p className="font-weight-bold text-dark">{startdate}</p>
                                             <hr />
                                             <ul>
                                                 <li className="listFrom text-success">{journey.jstartloc}</li>
-                                                <p className="lbltime font-italic">8.45am</p>
+                                                <p className="lbltime font-italic">{starttime}</p>
                                             </ul>
                                             <ul>
                                                 <li className="listFrom text-active">{journey.jendloc}</li>
-                                                <p className="lbltime font-italic">9.15am</p>
+                                                <p className="lbltime font-italic">{endtime}</p>
                                             </ul>
                                             <div className="text-right bg-secondary">
                                                 <p className="lbljfare text-white">Rs. {journey.jfare}</p>
@@ -71,6 +109,27 @@ class TableCompleted extends Component {
         return (
             <div>
                 {table}
+
+                <div class="modal fade" id="viewTicketModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <h4>ID : { this.state.journeyid}</h4>
+                        </div>
+                        <div class="modal-footer">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
             </div>
         );
 
