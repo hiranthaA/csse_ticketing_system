fd114683920b90ab4933010aed7d2e4e5cf5df30
Parakrama Shashitha <pshashitha@gmail.com>
2018-10-04 13:46:52 +0530

    Rechage

--- src/components/body/content/recharge/recharge.js
+++ src/components/body/content/recharge/recharge.js
@@ -20,7 +20,25 @@ class Recharge extends Component {
             phoneNo:"",
             amount:0,
             createAccount:false,
-            customerID:""
+            customerID:"",
+            loggedIn:false,
+        }
+    }
+    componentDidUpdate(){
+
+    }
+    componentWillMount(){
+
+    }
+    componentDidMount(){
+        if(this.props.loggeduser!==null){
+            console.log(this.props.loggeduser);
+            this.setState({phoneNo:this.props.loggeduser.mobile,loggedIn:true});
+            document.getElementById("userMobile").value=this.props.loggeduser.mobile;
+        }else{
+
+            alert("You are not logged in!");
+            this.setState({loggedIn:false})
         }
     }
     addAccount(e){
@@ -36,16 +54,7 @@ class Recharge extends Component {
         this.setState({code:code});
     }
     sendCode(e){
-        // fetch("https://api.txtlocal.com/send/",{method:"POST",body:{"apiKey":apiKey,"numbers":numbers,"sender":sender,"message":message}}).then((res)=>{
-        //     if(res.status==="success"){
-        //         alert("Verification Code Sen");
-        //     }
-        // });
-       /** 
-        //TWILIO
-        process.env.MY_PHONE_NUMBER            TO STORE MY NUMBER
-
-        */
+        
         alert(this.state.phoneNo);
         var body = {"accountQuantity":this.state.amount,"phoneNo":this.state.phoneNo};
         fetch("http://localhost:9090/accounts/sendSMS",{
@@ -136,17 +145,168 @@ class Recharge extends Component {
            
     }
     render() { 
+        var form;
+        if(this.state.loggedIn===false){
+            form = (
+                <form class="content">
+                            <div className="row rechargeRowClass">
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                    <br />
+                                    <br />
+                                    <br />
+                                    <p><small>Mobile Number</small></p>
+                                </div>
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                    <input type="number" className="rechargeInputs" id="userMobile" placeholder="Ex: 0771234567" onChange={this.handlePhone}></input>
+                                </div>
+                            </div> 
+                            <div className="row rechargeRowClass">
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                    <br />
+                                    <br />
+                                    <br />
+                                    <p><small>Recharge Amount</small></p>
+                                </div>
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                    
+                                        <div class="form-group form-inline">
+                                            <label class="sr-only" for="exampleInputAmount">Amount (in dollars)</label>
+                                            <div class="form-text-Recharge">
+                                                <input type="number" className="rechargeInputsAmount" placeholder="Ex: 100" id="ramount" min={0} onChange={this.handleAmount}/>
+                                                <label for="ramount" className="static-value-Recharge">Rs/=</label>
+                                            </div>
+                                        </div>
+                                
+                                
+                                </div>
+                            </div>
+                            <br/>
+                            <div className="row">
 
+                                <div className="col rechargeCodeClass">
+                                        <button type="submit" onClick={this.sendCode} class="btn btn-secondary btn-lg" disabled={true}>Get Code</button> 
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                    <br />
+                                    <br />
+                                    <br />
+                                    <p className="lead">Please enter the 4 digit code received to your mobile to recharge this amount.</p>
+                                </div>
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                    <div className="row">
+                                        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                                            <input type="number" className="verify-code-recharge rechargeInputs" placeholder="Enter your 4 digit Code Here" onChange={this.checkCode}></input>
+                                        </div>
+                                        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                                            <button type="button"   className="btn btn-success createAccountPay" disabled={true} onClick={this.validateVerification}>Submit Code</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <br/>
+                            <div className="row">
+
+                                <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+                                    
+                                    <h3 className="rechargeText"><font color="green">No Account!</font></h3>
+                                </div>
+                                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
+                                    <button type="button" className="btn btn-success createAccountPay" onClick={this.addAccount} disabled={true}>Create Account</button>
+                                </div>
+                                <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+                                    <button type="button" className="btn btn-info" onClick={() => this.props.setMainBodyContent("home")}>Go Back</button>
+                                </div>
+                            </div>
+                        </form> 
+            );
+        }else{
+            form = (
+                <form class="content">
+                            <div className="row rechargeRowClass">
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                    <br />
+                                    <br />
+                                    <br />
+                                    <p><small>Mobile Number</small></p>
+                                </div>
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                    <input type="number" className="rechargeInputs" id="userMobile" placeholder="Ex: 0771234567" onChange={this.handlePhone}></input>
+                                </div>
+                            </div> 
+                            <div className="row rechargeRowClass">
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                    <br />
+                                    <br />
+                                    <br />
+                                    <p><small>Recharge Amount</small></p>
+                                </div>
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                    
+                                        <div class="form-group form-inline">
+                                            <label class="sr-only" for="exampleInputAmount">Amount (in dollars)</label>
+                                            <div class="form-text-Recharge">
+                                                <input type="number" className="rechargeInputsAmount" placeholder="Ex: 100" id="ramount" min={0} onChange={this.handleAmount}/>
+                                                <label for="ramount" className="static-value-Recharge">Rs/=</label>
+                                            </div>
+                                        </div>
+                                
+                                
+                                </div>
+                            </div>
+                            <br/>
+                            <div className="row">
+
+                                <div className="col rechargeCodeClass">
+                                        <button type="submit" onClick={this.sendCode} class="btn btn-secondary btn-lg">Get Code</button> 
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                    <br />
+                                    <br />
+                                    <br />
+                                    <p className="lead">Please enter the 4 digit code received to your mobile to recharge this amount.</p>
+                                </div>
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                    <div className="row">
+                                        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                                            <input type="number" className="verify-code-recharge rechargeInputs" placeholder="Enter your 4 digit Code Here" onChange={this.checkCode}></input>
+                                        </div>
+                                        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                                            <button type="button"   className="btn btn-success createAccountPay" onClick={this.validateVerification}>Submit Code</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <br/>
+                            <div className="row">
+
+                                <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+                                    
+                                    <h3 className="rechargeText"><font color="green">No Account!</font></h3>
+                                </div>
+                                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
+                                    <button type="button" className="btn btn-success createAccountPay" onClick={this.addAccount}>Create Account</button>
+                                </div>
+                                <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+                                    <button type="button" className="btn btn-info" onClick={() => this.props.setMainBodyContent("home")}>Go Back</button>
+                                </div>
+                            </div>
+                        </form> 
+            );
+        }
         let addAccount;
         if(this.state.createAccount!==false){
             
-            addAccount =(<Account account={this.state} loggedUser={this.props.loggedUser}/>);
+            addAccount =(<Account account={this.state} loggeduser={this.props.loggeduser}/>);
         }
-
+        var account = this.state.customerID.substring(0,3)+this.state.phoneNo.substring(3,9)
         let currentBalance=""+this.state.balance;
-        let currentAccount=""+this.state.account.toString().substring(0,3)+" "
-                                        +this.state.account.toString().substring(3,6)+" "
-                                            +this.state.account.toString().substring(6,9);
+        let currentAccount=""+account.substring(0,3)+" "
+                                        +account.substring(3,6)+" "
+                                            +account.toString().substring(6,9);
         let phoneNum = ""+this.state.phoneNo;
         let div=(<div></div>);
         let bal = (<div></div>);
@@ -158,6 +318,7 @@ class Recharge extends Component {
                 bal=(<h5>Balance:    <small>{currentBalance}</small></h5>);
             if(this.state.account!=="")
                 acc=(<h5>Account No: <small>{currentAccount}</small></h5>);
+            
             div =       (
                                         <div className="row justify-content-left">
                                         <div className="col">
@@ -177,8 +338,8 @@ class Recharge extends Component {
         }
         return ( 
             <div>
-                <div className="reChargeAccount">
-                    <div className="row">
+                <div className="reChargeAccount card bg-light border-info d-flex p-2">
+                    <div className="row card-header bg-info">
                         <div className="col">
                             <h4>ACCOUNT RECHARGE</h4>
                             <br />
@@ -187,78 +348,9 @@ class Recharge extends Component {
                         </div>
                     </div>
                     {div}
-                    <form class="content">
-                        <div className="row rechargeRowClass">
-                            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                <br />
-                                <br />
-                                <br />
-                                <p><small>Mobile Number</small></p>
-                            </div>
-                            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                <input type="number" className="rechargeInputs" placeholder="Ex: 0771234567" onChange={this.handlePhone}></input>
-                            </div>
-                        </div> 
-                        <div className="row rechargeRowClass">
-                            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                <br />
-                                <br />
-                                <br />
-                                <p><small>Recharge Amount</small></p>
-                            </div>
-                            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                
-                                    <div class="form-group form-inline">
-                                        <label class="sr-only" for="exampleInputAmount">Amount (in dollars)</label>
-                                        <div class="form-text-Recharge">
-                                            <input type="number" className="rechargeInputsAmount" placeholder="Ex: 100" id="ramount" min={0} onChange={this.handleAmount}/>
-                                            <label for="ramount" className="static-value-Recharge">Rs/=</label>
-                                        </div>
-                                    </div>
-                             
-                            
-                            </div>
-                        </div>
-                        <br/>
-                        <div className="row">
-
-                            <div className="col rechargeCodeClass">
-                                    <button type="submit" onClick={this.sendCode} class="btn btn-secondary btn-lg">Get Code</button> 
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                <br />
-                                <br />
-                                <br />
-                                <p className="lead">Please enter the 4 digit code received to your mobile to recharge this amount.</p>
-                            </div>
-                            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                <div className="row">
-                                    <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-                                        <input type="number" className="verify-code-recharge rechargeInputs" placeholder="Enter your 4 digit Code Here" onChange={this.checkCode}></input>
-                                    </div>
-                                    <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-                                        <button type="button"   className="btn btn-success createAccountPay" onClick={this.validateVerification}>Submit Code</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <br/>
-                        <div className="row">
-
-                            <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
-                                
-                                <h3><font color="green">No Account!</font></h3>
-                            </div>
-                            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
-                                <button type="button" className="btn btn-info createAccountPay" onClick={this.addAccount}>Create Account</button>
-                            </div>
-                            <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
-                                <button type="button" className="btn btn-info" onClick={() => this.props.setMainBodyContent("home")}>Go Back</button>
-                            </div>
-                        </div>
-                    </form> 
+                        <div className="card-body">
+                        {form}
+                    </div>
                 </div>
                 {addAccount}
             </div>
