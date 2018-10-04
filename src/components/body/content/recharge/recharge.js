76bab014a1b94d2b0e18754972fae29880ee7b3c
Parakrama Shashitha <pshashitha@gmail.com>
2018-10-04 14:54:01 +0530

    Get Code button fix

--- src/components/body/content/recharge/recharge.js
+++ src/components/body/content/recharge/recharge.js
@@ -212,11 +212,11 @@ class Recharge extends Component {
                                     
                                     <h3 className="rechargeText"><font color="green">No Account!</font></h3>
                                 </div>
-                                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
+                                <div className="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-5">
                                     <button type="button" className="btn btn-success createAccountPay" onClick={this.addAccount} disabled={true}>Create Account</button>
                                 </div>
-                                <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
-                                    <button type="button" className="btn btn-info" onClick={() => this.props.setMainBodyContent("home")}>Go Back</button>
+                                <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+                                    <button type="button" className="btn btn-info createAccountPay" onClick={() => this.props.setMainBodyContent("home")}>Go Back</button>
                                 </div>
                             </div>
                         </form> 
@@ -287,11 +287,11 @@ class Recharge extends Component {
                                     
                                     <h3 className="rechargeText"><font color="green">No Account!</font></h3>
                                 </div>
-                                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
+                                <div className="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-5">
                                     <button type="button" className="btn btn-success createAccountPay" onClick={this.addAccount}>Create Account</button>
                                 </div>
-                                <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
-                                    <button type="button" className="btn btn-info" onClick={() => this.props.setMainBodyContent("home")}>Go Back</button>
+                                <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+                                    <button type="button" className="btn btn-info createAccountPay" onClick={() => this.props.setMainBodyContent("home")}>Go Back</button>
                                 </div>
                             </div>
                         </form> 
